7b05ebac1c725fbf1f17a8d01fb551057cf78d37
Italo Vinicius <italoviniciusrope@gmail.com>
2025-08-24 21:04:18 -0300

    fix: correcao nome funcao

--- Atividade Avaliativa 1/src/Exercicio4.java
+++ Atividade Avaliativa 1/src/Exercicio4.java
@@ -1,12 +1,12 @@
 public class Exercicio4 {
     public static void main(String[] args) {
         int n = 5;
-        System.out.println("O " + n + "º número da sequência de Pell é: " + calcularSequenciaDepell(n));
+        System.out.println("O " + n + "º número da sequência de Pell é: " + calcularSequenciaDePell(n));
     }
 
-    public static int calcularSequenciaDepell(int n) {
+    public static int calcularSequenciaDePell(int n) {
         if (n == 0 || n == 1) return n;
 
-        return 2 * calcularSequenciaDepell(n - 1) + calcularSequenciaDepell(n - 2);
+        return 2 * calcularSequenciaDePell(n - 1) + calcularSequenciaDePell(n - 2);
     }
 }
